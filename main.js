50dce5d5d4cb540392ea6b060405d9f4be67ac89
simonearata <simone.arata1@gmail.com>
2021-04-19 18:17:21 +0200

    first

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+
+  // creo referenze agli elementi
+  var menu_hidden = $(".header-right");
+  var menu_ham = $(".hamburger-menu");
+  var bottone = $(".header-left");
+
+  // menu principale nascosto
+  menu_hidden.hide();
+
+  // flag 
+  var testo_visibile = false;
+
+  // clicco sul tanto boolean e rendo visibile / nascosto il testo
+
+  bottone.click(function(){
+
+    if(testo_visibile === false){
+      menu_hidden.show();
+      testo_visibile = true;
+
+    }else{
+      menu_hidden.hide();
+      testo_visibile = false
+    }
+
+
+  })
+
+
+
+})
